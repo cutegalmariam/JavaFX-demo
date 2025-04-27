13d4de74b9ee3f815f7edde5938375ee03457b28
Mariam Kavtaradze <mariakavt@gmail.com>
2025-04-27 12:26:16 +0400

    add test app logic

--- src/main/java/org/example/demo2/HelloApplication.java
+++ src/main/java/org/example/demo2/HelloApplication.java
@@ -1,8 +1,9 @@
 package org.example.demo2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +11,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Button button = new Button("Click me!");
+        button.setOnAction(e -> {
+            System.out.println("button clicked!");
+        });
+
+        StackPane root = new StackPane();
+
+        root.getChildren().add(button);
+
+        Scene scene = new Scene(root, 300, 300);
+        stage.setTitle("My JavaFX app!");
+        stage.setScene(scene);
+
+        stage.show();
 
     }
 
